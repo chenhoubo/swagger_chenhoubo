28e850cc4ecefa3f8230791fc8947322cd0efc37
briup2 <briup2@ab07e92d-4b7f-4665-a4f6-c0552421599c>
2019-05-10 11:30:41 +0000

    后台增加了微信小程序的控制类

---
+++ STSchb/app01/src/main/java/com/briup/apps/app01/web/controller/TestController.java
@@ -0,0 +1,83 @@
+package com.briup.apps.app01.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.briup.apps.app01.util.AesUtil;
+import com.briup.apps.app01.util.HttpRequest;
+
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping("/wx")
+public class TestController {
+	
+	@ApiOperation("获取微信小程序的openid")
+	@ResponseBody
+	@RequestMapping(value = "/getOpenid", method = RequestMethod.GET)
+    public Map getOpenid(String code, String encryptedData, String iv ) throws Exception{
+		
+		Map<String,Object> map = new HashMap<String,Object>();
+		//code = "081ZExyD0qnP4j2LV5yD0hFLyD0ZExyK";
+        //登录凭证不能为空
+        if (code == null || code.length() == 0) {
+            map.put("status", 0);
+            map.put("msg", "code 不能为空");
+            System.out.println("map1:" + map);
+            return map;
+        }
+        //小程序唯一标识   (在微信小程序管理后台获取)
+        String wxspAppid = "wxa9d9ad7c4fbd2dfd";
+        //小程序的 app secret (在微信小程序管理后台获取)
+        String wxspSecret = "feff454362ded21ce1cff077d84a890f";
+        //授权（必填）
+        String grant_type = "authorization_code";
+        //////////////// 1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid ////////////////
+        //请求参数
+        String params = "appid=" + wxspAppid + "&secret=" + wxspSecret + "&js_code=" + code + "&grant_type=" + grant_type;
+        //发送请求
+        String sr = HttpRequest.sendGet("https://api.weixin.qq.com/sns/jscode2session", params);
+        //解析相应内容（转换成json对象）
+        JSONObject json = JSONObject.parseObject(sr);
+        //获取会话密钥（session_key）
+        String session_key = json.get("session_key").toString();
+        //用户的唯一标识（openid）
+        String openid = (String) json.get("openid");
+        System.out.println("openid:" + openid);
+        //////////////// 2、对encryptedData加密数据进行AES解密 ////////////////
+        try {
+            String result = AesUtil.decrypt(encryptedData, session_key, iv, "UTF-8");
+            if (null != result && result.length() > 0) {
+                map.put("status", 1);
+                map.put("msg", "解密成功");
+ 
+                JSONObject userInfoJSON = JSONObject.parseObject(result);
+                Map<String,Object> userInfo = new HashMap<String,Object>();
+                userInfo.put("openId", userInfoJSON.get("openId"));
+                userInfo.put("nickName", userInfoJSON.get("nickName"));
+                userInfo.put("gender", userInfoJSON.get("gender"));
+                userInfo.put("city", userInfoJSON.get("city"));
+                userInfo.put("province", userInfoJSON.get("province"));
+                userInfo.put("country", userInfoJSON.get("country"));
+                userInfo.put("avatarUrl", userInfoJSON.get("avatarUrl"));
+                userInfo.put("unionId", userInfoJSON.get("unionId"));
+                map.put("userInfo", userInfo);
+                System.out.println("map2:" + map);
+                return map;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        map.put("status", 0);
+        map.put("msg", "解密失败");
+        System.out.println("map3:" + map);
+        return map;
+    }
+}
